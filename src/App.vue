872477467faa942aa1260ecbe79168b7ad899233
RixTerreny <riccardotrave43@gmail.com>
2022-12-02 16:16:04 +0100

    draft bug

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    components: (TheHeader,TheFooter)
+}
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TheFooter from './components/TheFooter.vue';
+import TheHeader from './components/TheHeader.vue';
+</script>
+
+<template>
+  <TheHeader></TheHeader>
+  <div>
+    fsdfgs
+  </div>
+</template>
+
+<style lang="scss">
+  @use './styles/general.scss' as *;
+  div{
+    background-color: $gg;
+  }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<script>
+export default{}
+</script>
+
+<template>
+    <div>
+        fgsdufsdhfsdafus
+    </div>
+</template>
+
+
+<style lang="scss">
+@use '../styles/general.scss' as *;
+div{
+    color: $gg;
+}
+</style>
